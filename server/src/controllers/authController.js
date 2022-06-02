caafdfffd8d50ef2a5ae2e366e2f8d71d672eba1
dunghenry <trandungksnb6930082001@gmail.com>
2022-06-02 17:55:49 +0700

    register and login

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,18 +1,60 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const bycrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const authController = {
-    register: async (req, res) => {
-        
-    },
-    generateAccessToken: () => {
-        
-    },
-    login: async (req, res) => {
-
-    },
-    loginWithGoogle: async (req, res) => {
-        
+  generateAccessToken: (user) => {
+    return jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+  },
+  register: async (req, res) => {
+    const { email, firstName, lastName } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ message: "User already exists!" });
+      }
+      const salt = await bycrypt.genSalt(10);
+      const hashedPassword = await bycrypt.hash(req.body.password, salt);
+      const newUser = new User({
+        email,
+        password: hashedPassword,
+        name: `${firstName} ${lastName}`,
+      });
+        const userSaved = await newUser.save();
+        const { password, ...infoUser } = userSaved._doc;
+      return res.status(201).json({...infoUser});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
     }
-
-}
+  },
+    login: async (req, res) => {
+      const {email} = req.body;
+      try {
+          const user = await User.findOne({ email });
+          if (!user) {
+            return res.status(404).json({ message: "User doesn't exist" });
+          }
+          const isValidPassword = await bycrypt.compare(req.body.password, user.password);
+          if (!isValidPassword) {
+            return res.status(400).json({ message: "Password is incorrect" });
+          }
+          const accessToken = authController.generateAccessToken(user);
+          const { password, ...infoUser } = user._doc;
+          return res.status(200).json({...infoUser, accessToken})
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: error.message });
+    }
+  },
+    loginWithGoogle: async (req, res) => {
+      
+  },
+};
 
 module.exports = authController;
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: false
+    },
+    googleId: {
+        type: String,
+        required: false
+    },
+    id: {type: String}
+}, {
+    timestamps: true
 })
-
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
